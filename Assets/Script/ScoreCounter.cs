0eb9931f289d13859db6257744c51b4799377d6f
TreeFolkDruid <60739585+TreeFolkDruid@users.noreply.github.com>
2020-02-06 15:30:04 +0100

    ScoreCounter
    Highscore finns nu

---
+++ Assets/Script/ScoreCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    float currentScore = 0;
+    
+    [SerializeField] Text score; //The text projects the players highscore.
+    private bool isEnabled = true; // A bool that will activate the scorecounting
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = FindObjectOfType<Text>(); //getting the text compoment.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnabled) // Activates the scorecounting.
+        {
+            currentScore += 1 * Time.deltaTime;
+            score.text = $"Highscore: " + Mathf.FloorToInt(currentScore);
+        }
+    }
+
+}
