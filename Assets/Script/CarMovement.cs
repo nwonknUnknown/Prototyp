88f40317e2c3a119fd613d953f2e19f8fb4ad42d
nwonknUnknown <59011220+nwonknUnknown@users.noreply.github.com>
2020-02-07 12:01:47 +0100

    movement update

---
+++ Assets/Script/CarMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+
+    public float horizVelocity = 0;
+    public string moveLock = "n";
+    public int laneNumber = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(horizVelocity, 0, 0);
+
+        if ((Input.GetKeyDown(moveLeft) && (laneNumber > 0) && (moveLock == "n")))
+        {
+            horizVelocity = -2;
+            StartCoroutine(stopSlide());
+            laneNumber -= 1;
+            moveLock = "y";
+        }
+        if ((Input.GetKeyDown(moveRight) && (laneNumber < 4) && (moveLock == "n")))
+        {
+            horizVelocity = +2;
+            StartCoroutine(stopSlide());
+            laneNumber += 1;
+            moveLock = "y";
+        }
+    }
+    IEnumerator stopSlide()
+    {
+        yield return new WaitForSeconds(0.5f);
+        horizVelocity = 0;
+        moveLock = "n";
+    }
+}
