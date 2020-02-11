517ad3641e06fbd81cc3a1fc329482fae064b92c
TreeFolkDruid <60739585+TreeFolkDruid@users.noreply.github.com>
2020-02-11 09:25:12 +0100

    Update States.cs

--- Assets/Script/States.cs
+++ Assets/Script/States.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class States : MonoBehaviour
 {
+    public virtual States Do() {
+        return this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+    class WaitState : States
+    {
+        public override States Do()
+        {
+            return this;
+        }
+    }
+
+    class FrameBlock : States
+    {
+
+    }
+
+
+
+
