fed4efad76eaa603713cd7fd29e06e56818be34b
TreeFolkDruid <60739585+TreeFolkDruid@users.noreply.github.com>
2020-02-11 13:35:11 +0100

    Update GameManager.cs

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        state.
+
     }
 
     // Update is called once per frame
