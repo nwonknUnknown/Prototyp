cbec677e601c5d09fc630318decf3109348bebae
nwonknUnknown <59011220+nwonknUnknown@users.noreply.github.com>
2020-02-11 12:17:15 +0100

    GameOverSceneUpdate_1

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("LevelName");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
